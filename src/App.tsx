bc24dd419246fdb958d931b01ce2f8398693472e
Lucas M <lucasmmororo@gmail.com>
2025-07-25 16:51:16 -0300

    Cfg tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-slate-900 border">
+    <div className="flex h-screen items-center justify-center bg-red-900 border">
       <h1 className="text-3xl text-white underline">
         Olá, Tailwind CSS com Vite!
       </h1>
